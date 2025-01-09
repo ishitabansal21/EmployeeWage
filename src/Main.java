8208748186cab3473e53702f1ebb47312c9973a2
Ishita <ishitabansal2102@gmail.com>
2025-01-09 22:34:22 +0530

    Added:Check attendance

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee e1=  new Employee("Ram",123);
+        Random r = new Random();
+        int i = (r.nextInt(1000))%2;
+        switch (i){
+            case 1 -> e1.attendance(true);
+            default -> e1.attendance(false);
+        }
+    }
+}
