0c5ed79e5099246b19664976e28c6bf334e897b2
Ishita <ishitabansal2102@gmail.com>
2025-01-10 17:30:37 +0530

    Added:PartTimeWage

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Employee e1=  new Employee("Ram",123);
         Random r = new Random();
-        int i = (r.nextInt(1000))%2;
-        switch (i){
+       // int i = (r.nextInt(1000))%2;
+        int attendanceType = r.nextInt(3);
+        switch (attendanceType) {
             case 1 -> {
                 e1.attendance(true);
-                e1.calculateDailyWage();
+                e1.calculateDailyWage("full-time");
+            }
+            case 2 -> {
+                e1.attendance(true);
+                e1.calculateDailyWage("part-time");
             }
             default -> e1.attendance(false);
         }
